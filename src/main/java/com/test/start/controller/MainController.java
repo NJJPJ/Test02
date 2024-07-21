69277c8bdd73c130ce6ab717270657b7b324fec6
Jin <sebgokti4276@naver.com>
2024-07-21 16:49:33 +0900

    dd

--- src/main/java/com/test/start/controller/MainController.java
+++ src/main/java/com/test/start/controller/MainController.java
@@ -9,7 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 	
 	@GetMapping("/index")
-	public String goIndex() {
+	public String goIndex() {	
 		return "주말마다 항상 스펙타클하고 파란만장하고 아주 그냥 도파민덩어리";
+	}
+	
+    @GetMapping("/jin")
+    public String goRest() {
+        return "레스트 에이피아이";
+    }
+	
+	@GetMapping("/")
+	public String main() {
+		return "월요일은 힘들다";
 	}
 }
